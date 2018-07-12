73b47b15029bcf281a4252ac73c9a4ac99b92d0e
Unknown <kjhop@wp.pl>
2018-07-12 23:59:24 +0200

    11 - introduction to express

--- 01/App.js
+++ 01/App.js
@@ -1,26 +1,13 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res)=>{
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    }else if(req.url === '/contact'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res);
-    }else if(req.url === '/api/people'){
-        const people = [
-            {name: 'robin', age:26},
-            {name: 'kot', age: 36}
-        ]
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(people));
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/error.html').pipe(res);
-    }
+const app = express();
+
+app.get('/', (req, res) =>{
+     res.send('Gitara siema');
 });
 
-server.listen(8080, '127.0.0.1');
-console.log("Listening...");
+app.get('/contact', (req, res) =>{
+    res.send('Gitara siema!');
+});
+
+app.listen(8080);
