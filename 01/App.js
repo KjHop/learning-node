8156f6f8dbe13ce52434d8a90acff42c6e043886
Unknown <kjhop@wp.pl>
2018-07-12 23:36:40 +0200

    10 - basic routing

--- 01/App.js
+++ 01/App.js
@@ -3,13 +3,23 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    const myObj ={
-        name: "Kacper",
-        job: "Junior Frontend Developer",
-        age: 18
-    };
-    res.end(JSON.stringify(myObj));
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }else if(req.url === '/api/people'){
+        const people = [
+            {name: 'robin', age:26},
+            {name: 'kot', age: 36}
+        ]
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(people));
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/error.html').pipe(res);
+    }
 });
 
 server.listen(8080, '127.0.0.1');
