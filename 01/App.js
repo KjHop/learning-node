3c6d141614509a465175c87afc1be6f049341fde
Unknown <kjhop@wp.pl>
2018-07-12 23:18:38 +0200

    09 - passing json

--- 01/App.js
+++ 01/App.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const myObj ={
+        name: "Kacper",
+        job: "Junior Frontend Developer",
+        age: 18
+    };
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(8080, '127.0.0.1');
