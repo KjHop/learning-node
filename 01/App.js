5dac5b02aacbdff94d038571730b1bb04b916307
Unknown <kjhop@wp.pl>
2018-07-12 22:52:32 +0200

    07 - streams

--- 01/App.js
+++ 01/App.js
@@ -1,5 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
+let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt', 'utf8');
+
+myReadStream.on('data', chunk =>{
+    console.log('new chunk received: ');
+    myWriteStream.write(chunk);
+});
+
+/*
 const server = http.createServer((req, res)=>{
     console.log('request was made: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -8,3 +18,4 @@ const server = http.createServer((req, res)=>{
 
 server.listen(8080, '127.0.0.1');
 console.log("Listening...");
+*/  
