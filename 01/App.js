c871c3c2fe532d9548158c0a7a1cb88ba9074b23
Unknown <kjhop@wp.pl>
2018-07-12 23:10:09 +0200

    08 - pipes and serving html pages

--- 01/App.js
+++ 01/App.js
@@ -1,21 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
-let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt', 'utf8');
-
-myReadStream.on('data', chunk =>{
-    console.log('new chunk received: ');
-    myWriteStream.write(chunk);
-});
-
-/*
 const server = http.createServer((req, res)=>{
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hey there");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(8080, '127.0.0.1');
 console.log("Listening...");
-*/  
