d4b0770c59d3fa1f939bb511673cd9a84869cc29
Unknown <kjhop@wp.pl>
2018-07-13 22:38:41 +0200

    12 - routing parameters

--- 01/App.js
+++ 01/App.js
@@ -10,4 +10,8 @@ app.get('/contact', (req, res) =>{
     res.send('Gitara siema!');
 });
 
+app.get('/profile/:id', (req, res) =>{
+    res.send('You requested to see a profile with the id of ' + req.params.id);
+});
+
 app.listen(8080);
